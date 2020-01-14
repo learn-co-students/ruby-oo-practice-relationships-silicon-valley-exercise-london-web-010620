243695baffae975978aa6e88c4b494b39ad7abbc
Samuel Kyprianou <samuelkyprianou@gmail.com>
2020-01-14 21:15:51 +0000

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,37 @@
 class VentureCapitalist
+    attr_accessor :total_worth
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select { |vp| vp.total_worth > 1000000000 }
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select { |fr| fr.venture_capitalist == self }
+    end
+
+    def portfolio
+        self.funding_rounds.map { |fr| fr.startup }.uniq
+    end
+
+    def biggest_investment
+        self.funding_rounds.max { |fr1, fr2| fr1.investment <=> fr2.investment }
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Startup.new("Revolut", "John", "Finance")
+s2 = Startup.new("Igloo",	"Jane", "Energy")
+s3 = Startup.new("Trouva", "Tom", "Retail")
+
+vc1 = VentureCapitalist.new("Perfect Partners", 600000)
+vc2 = VentureCapitalist.new("Virtuous Ventures", 30000000)
+vc3 = VentureCapitalist.new("Free For All Fund", 1000000000)
+
+fr1 = FundingRound.new(s1, vc1, "Angel", 2000)
+fr2 = FundingRound.new(s2, vc1, "Seed", 1000)
+fr3 = FundingRound.new(s3, vc2, "Series A", 3000)
+fr4 = FundingRound.new(s2, vc3, "Angel", 5000)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
