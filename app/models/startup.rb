5f99ab01a702abc4a5d07cba045904e6e864d2ab
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-14 16:35:36 +0000

    done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,58 @@
 class Startup
 
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+
+    def initialize(name,founder,domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.find_by_founder(founder)
+        self.all.find {|startup| startup.founder == founder}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(new_domain,startup_name)
+        self.name = startup_name
+        self.domain = new_domain
+    end
+
+    def self.domains
+        self.all.map {|startup| startup.domain}
+    end
+
+    def sign_contract(vc,type_of_investment,amount)
+        FundingRound.new(vc,type_of_investment,amount,self)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select {|round| round.startup == self}.length
+    end
+
+    def total_funds
+        FundingRound.all.select {|round| round.startup == self}.map {|investment| investment.investment}.sum
+    end
+
+    def investors
+        FundingRound.all.select {|round| round.startup == self}.map {|round| round.venture_capitalist}.uniq
+    end
+
+    def big_investors
+            # investors.select {|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+            big_investors = VentureCapitalist.tres_commas_club.each do |member| 
+                if member.name == investors.any?
+                    member
+                end
+            end  
+            big_investors         
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,21 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+vc_a = VentureCapitalist.new("vc_11",100)
+vc_b = VentureCapitalist.new("vc_22",200) 
+vc_c = VentureCapitalist.new("vc_66",2000000000000) 
+vc_d = VentureCapitalist.new("vc_33",3000000000)
+
+startup_AA = Startup.new("startup_11","founder_AAA","domain_AAA")
+startup_BB = Startup.new("startup_22","founder_BBB","domain_BBB")
+startup_CC = Startup.new("startup_33","founder_CCC", "domain_CCC")
+
+round_1 = FundingRound.new(vc_d,"round_A",100,startup_AA)
+round_2 = FundingRound.new(vc_b,"round_A",20,startup_BB)
+round_3 = FundingRound.new(vc_a,"round_B",30,startup_AA)
+round_4 = FundingRound.new(vc_b,"round_D",40,startup_CC)
+round_4 = FundingRound.new(vc_d,"round_D",40,startup_AA)
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+
+puts "works!"
